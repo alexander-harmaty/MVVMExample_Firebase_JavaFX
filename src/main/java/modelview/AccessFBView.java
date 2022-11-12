7251f8941f961e49563d9d7f21cf1ce2d6c8515b
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-11-12 00:47:56 -0500

    latest

--- src/main/java/modelview/AccessFBView.java
+++ src/main/java/modelview/AccessFBView.java
@@ -25,14 +25,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import models.Person;
-import org.checkerframework.checker.units.qual.s;
 
 public class AccessFBView implements Initializable {
 
